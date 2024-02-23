3538b155c730535a8888da8fa51f0a5850b007f7
thecybercroc <thecybercroc@gmail.com>
2024-02-23 16:45:24 +0530

    Adding data to database and create a new database instance with the given data and options.

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello Prem!");
-});
+app.use(express.json());
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth.js'));
+app.use('/api/notes', require('./routes/notes.js'));
+
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
